a6b801d87d0b509271585b6f8dea9823a440468d
Avula.Sai Venkata Harika <harikaavula76@gmail.com>
2026-06-12 10:33:54 +0530

    Add files via upload

---
+++ Day-12/assert.py
@@ -0,0 +1,21 @@
+#assert
+'''
+status= None
+assert satus !=None, "You need to update the status"
+print(satus)
+'''
+'''
+name='abc'
+batch=55
+age=21
+assert(name!='' and batch!=0 and age!=0),"You need to update the date"
+print(name,batch,age)
+'''
+'''
+name='abc'
+batch=55
+age=21
+assert(name!=None and batch!=None and age!=None),"You need to update the date"
+print(name,batch,age)
+''''
+
---
+++ Day-12/while conditions.py
@@ -0,0 +1,100 @@
+'''Int
+While condition:
+Upd
+#start
+'''
+'''
+i=1
+while i<11:
+    print(i)
+    i+=1'''
+
+
+
+
+'''
+i=2
+while i<21:
+    print(i)
+    i+=2'''
+
+
+
+
+'''
+i=10
+while i>0:
+    print(i)
+    i-=1
+'''
+
+'''
+i=5
+while i<51:
+    print(i)
+    i+=5
+'''
+
+'''
+l=[1,2,4,5,6,7,3,9]
+i=0
+while i <len(l):
+    print(l[i])
+    i+=1
+    '''
+'''
+l='python programming'
+i=0
+while i <len(l):
+    print(l[i])
+    i+=1
+
+
+l=(1,2,3,4,4,5,6,7)
+i=0
+while i <len(l):
+    print(l[i])
+    i+=1
+  '''
+
+'''
+#removing zeros from list example
+l=[1,0,0,2,3,4,0,0,0,1,0,3,50,2,0,64,23,0,0,0,0,0,12,3,34]
+while 0 in l:
+    l.remove(0)
+print(l)'''
+
+'''
+moves=30
+while moves>1:
+    status = input("[W] in or [c]ontinue")
+    if status =='W':
+        print("You won the game")
+        break
+    moves-=1
+    print(f'{moves} moves are left')
+
+else:
+    print("game over")'''
+
+
+
+
+bullets=30
+while bullets>1:
+    status = input("[W] in or [c]ontinue")
+    if status =='W':
+        print("You won the game")
+        break
+    bullets-=1
+    print(f'{bullets} moves are left')
+
+else:
+    print("game over")
+        
+        
+    
+
+
+
+
